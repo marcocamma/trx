ddd90066214a4e4248e9da2128c7a9295df9c821
marco cammarata <marcocamma@gmail.com>
2017-12-03 04:26:22 +0100

    added find_packages to setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 def readme():
     with open('README.rst') as f:
         return f.read()
 
 setup(name='trx',
-      version='0.5.11',
+      version='0.5.12',
       description='tools for (T)ime (R)esolved (X)-ray techniques',
       long_description=readme(),
       url='https://github.com/marcocamma/trx',
       author='marco cammarata',
       author_email='marcocamma@gmail.com',
       license='MIT',
-      packages=['trx'],
+#      packages=['trx'],
+      packages=find_packages("."),
       install_requires=[
           'numpy',
           'fabio',
