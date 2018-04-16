3ac7f247c88a56e0197699bafc410ffee983e55b
marco cammarata <marcocamma@gmail.com>
2018-04-16 15:32:58 +0200

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='trx',
-      version='0.5.16',
+      version='0.6.0',
       description='tools for (T)ime (R)esolved (X)-ray techniques',
       long_description=readme(),
       url='https://github.com/marcocamma/trx',
